69693e14bff68e7c5b890596626a919487e2d53c
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-02-19 15:54:30 +0000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,25 +9,30 @@
 #include "sorting.h"
 using namespace std;
 
-int main(int argc, char * argv[]) {
-    
- 
-  cout<<"argc is "<<argc<<endl;
-    int i,N;
-    int *a;
-    for(i=1;i<argc;i++){
-        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
-      
-    }
-    /* Convert to numbers*/
-    N=argc-1;
-    cout<<"Before sorting:";
-    display(a,N);
-    
-    selectionsort(a,N);
-    cout<<"After sorting:";
-    display(a,N);
-    
-    
-    return 0;
+int main(int argc, char *argv[])
+{
+
+  cout << "argc is " << argc << endl;
+  int i, N;
+  int *a;
+  for (i = 1; i < argc; i++)
+  {
+    cout << "argv[" << i << "]" << argv[i] << endl;
+  }
+
+  /* Convert to numbers*/
+  N = argc - 1;
+  a = new int[argc - 1];
+  for (i = 0; i < N; i++)
+  {
+    a[i] = atoi(argv[i + 1]);
+  }
+  cout << "Before sorting:";
+  display(a, N);
+
+  sort(a, N);
+  cout << "After sorting:";
+  display(a, N);
+
+  return 0;
 }
